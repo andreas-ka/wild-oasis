a58992b353a2e417224f12d1e0dcbd334c7d5a3d
andreas-ka <andreas.krln@hotmail.com>
2023-10-23 14:36:18 +0200

    Added function to add cabins with image

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://oitmcjoqudswoflqqwwr.supabase.co";
+export const supabaseUrl = "https://oitmcjoqudswoflqqwwr.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9pdG1jam9xdWRzd29mbHFxd3dyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTc3MTY1OTEsImV4cCI6MjAxMzI5MjU5MX0.mPbJBtnZHNwEU2eczmEIOBFDjjFQhuprULhMdd-O-bE";
 const supabase = createClient(supabaseUrl, supabaseKey);
